6ad441c29c5bcbda90b9edd99a2f72afcc4cb64b
Claudio Amilton <pokety@gmail.com>
2025-08-09 08:33:46 -0300

    09/08

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 use std::collections::{ HashMap, HashSet};
-use std::thread::sleep;
-use std::time::Duration;
+// use std::thread::sleep;
+// use std::time::Duration;
+use std::io::{stdout, Write};
+use termimad::crossterm::{
+    cursor::{ Hide, Show},
+    event::{
+        self,
+        Event,
+        KeyEvent,
+        KeyCode::*,
+    },
+    queue,
+    terminal::{
+        self,
+        Clear,
+        ClearType,
+        EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+    style::Color::*,
+};
+use termimad::*;
+
 use std::{thread, time};
 use std::io::{stdin};
 
@@ -15,6 +36,55 @@ extern crate goto;
 // use genpdf::{elements, Element};
 
 ///mods
+
+fn view_area() -> Area {
+    let mut area = Area::full_screen();
+    area.pad_for_max_width(120); // we don't want a too wide text column
+    area
+}
+fn run_app(skin: MadSkin ,md :&str) -> Result<(), Error> {
+    let mut w = stdout(); // we could also have used stderr
+    queue!(w, EnterAlternateScreen)?;
+    terminal::enable_raw_mode()?;
+    queue!(w, Hide)?; // hiding the cursor
+    let mut view = MadView::from(md.to_owned(), view_area(), skin);
+    loop {
+        view.write_on(&mut w)?;
+        w.flush()?;
+        match event::read() {
+            Ok(Event::Key(KeyEvent{code, ..})) => {
+                match code {
+                    Up => view.try_scroll_lines(-1),
+                    Down => view.try_scroll_lines(1),
+                    PageUp => view.try_scroll_pages(-1),
+                    PageDown => view.try_scroll_pages(1),
+                    _ => break,
+                }
+            }
+            Ok(Event::Resize(..)) => {
+                queue!(w, Clear(ClearType::All))?;
+                view.resize(&view_area());
+            }
+            _ => {}
+        }
+    }
+    terminal::disable_raw_mode()?;
+    queue!(w, Show)?; // we must restore the cursor
+    queue!(w, LeaveAlternateScreen)?;
+    w.flush()?;
+    Ok(())
+}
+
+fn make_skin() -> MadSkin {
+    let mut skin = MadSkin::default();
+    skin.table.align = Alignment::Center;
+    skin.set_headers_fg(AnsiValue(178));
+    skin.bold.set_fg(Yellow);
+    skin.italic.set_fg(Magenta);
+    skin.scrollbar.thumb.set_fg(AnsiValue(178));
+    skin.code_block.align = Alignment::Center;
+    skin
+}
 
 fn zip_document_array(document:mongodb::sync::Cursor<Document>)->HashMap<std::string::String, Vec<i32>>{
     
@@ -60,23 +130,33 @@ impl Preview {
             println!("{}",i);
         }
     }
+    pub fn _display_preview_str(&self)->String{
+        let mut concat: String=String::new();
+        for i in self.arr.clone(){
+            concat.push_str(format!("{} \n",i).as_str());
+            // println!("{}",i);
+        }
+        concat
+
+    }
 }
 ////Saida
 fn saida(coll: Collection<Document>)-> Result<(), Box<dyn std::error::Error>>{
+    
     let mut prev: Preview=Preview::new();
     let all = coll.find(doc! {}).run()?;    
     let mut eventos: Vec<String> = Vec::new();
     eventos.push("NOVO EVENTO".to_string());
     eventos.sort();
-
+    
     let resultadio = all.filter_map(|s| {
         s.ok().filter(|f| f.get("evento").unwrap().as_str().unwrap() != "deposito")
     });
-
+    
     for result in resultadio {
         eventos.push(result.get("evento").unwrap().as_str().unwrap().to_string());
     }
-
+    
     let lista_usuarios=vec!("Claudio".to_string(),"Eyler".to_string(),"Dourado".to_string());
     let select_usuario: Result<String, InquireError> = Select::new("",lista_usuarios.into_iter().collect() )
     .with_help_message(" ↑↓ ESCOLHA O EVENTO!!")
@@ -85,7 +165,7 @@ fn saida(coll: Collection<Document>)-> Result<(), Box<dyn std::error::Error>>{
     .inspect_err(|_f| {
         let _=main();
     });
-
+    
     let lista_eventos: HashSet<String> = eventos.into_iter().collect();
     let select_evento: Result<String, InquireError> = Select::new("", lista_eventos.into_iter().collect())
     .with_help_message(" ↑↓ ESCOLHA O EVENTO!!")
@@ -104,14 +184,14 @@ fn saida(coll: Collection<Document>)-> Result<(), Box<dyn std::error::Error>>{
     match select_evento {
         Ok(choice) =>
         {
-          let event_d: Result<_,_> =  match choice.as_str(){
+            let event_d: Result<_,_> =  match choice.as_str(){
                 "NOVO EVENTO"=>{ Text::new("Evento?").with_page_size(40).with_validator(validator).prompt().inspect_err(|_f| {
                     let _=main();
                 })},
                 "EXIT" => std::process::exit(0x0100),
                 _ => Ok(choice)
             };
-          
+            
             loop {
                 std::process::Command::new("clear").status().unwrap();
                 prev.display_preview();
@@ -130,7 +210,7 @@ fn saida(coll: Collection<Document>)-> Result<(), Box<dyn std::error::Error>>{
                             None =>{
                                 prev.add_preview(format!(r"{} --{color_red}Nao cadastrado!!!{color_reset}",&query));
                                 // println!(r"modelo:{color_red}Nao cadastrado!!!{color_reset}");
-                                sleep(Duration::from_secs(2));
+                                // sleep(Duration::from_secs(2));
                             }
                         }
                     },
@@ -149,13 +229,12 @@ Ok(())
 
 ////Entrada
 fn entrada(coll: Collection<Document>)-> Result<(), Box<dyn std::error::Error>>{
-
     let validator = |input: &str| if input.chars().count() < 1 {
-            std::process::Command::new("clear").status().unwrap();
-            let _ =main();
-            Ok(Validation::Invalid("nao encontrado".into()))
-            
-        } else {
+        std::process::Command::new("clear").status().unwrap();
+        let _ =main();
+        Ok(Validation::Invalid("nao encontrado".into()))
+        
+    } else {
         Ok(Validation::Valid)
     };
     
@@ -166,27 +245,27 @@ fn entrada(coll: Collection<Document>)-> Result<(), Box<dyn std::error::Error>>{
         
         let query = Text::new("Patrimonio?").with_page_size(40).with_validator(validator)
         .prompt();
-        
-        match query {
-            Ok(query) => {
-                 let cursor = coll.find_one_and_update(doc! { "patrimonio":&query },doc! {"$set": doc! {"evento": "deposito"}}).run()?;
-                
-                match &cursor {
-                    Some(doc)=>{
-                        println!(r"modelo:{color_cyan}{}{color_reset}", doc.get("modelo").unwrap().as_str().unwrap());
-                    },
-                    None =>{
-                        println!(r"modelo:{color_red}Nao cadastrado!!!{color_reset}");
-                    }
-                } 
-            },
-            _error =>{
-                status=false;
-                std::process::Command::new("clear").status().unwrap();
-                let _=main();
-            }
+    
+    match query {
+        Ok(query) => {
+            let cursor = coll.find_one_and_update(doc! { "patrimonio":&query },doc! {"$set": doc! {"evento": "deposito"}}).run()?;
+            
+            match &cursor {
+                Some(doc)=>{
+                    println!(r"modelo:{color_cyan}{}{color_reset}", doc.get("modelo").unwrap().as_str().unwrap());
+                },
+                None =>{
+                    println!(r"modelo:{color_red}Nao cadastrado!!!{color_reset}");
+                }
+            } 
+        },
+        _error =>{
+            status=false;
+            std::process::Command::new("clear").status().unwrap();
+            let _=main();
         }
     }
+}
 Ok(())
 }
 
@@ -222,57 +301,51 @@ fn procurar(coll :&Collection<Document> ) -> Result<(), Box<dyn std::error::Erro
             match choice.as_str() {
                     "patrimonio" =>{
                         gpoint!['begin:
-                        let query = Text::new("Patrimonio/modelo?").with_page_size(40).with_validator(validator)
-                            .prompt()
-                            .inspect_err(|_f| {
-                                let _=main();
-                            });
-                        let cursor = coll.find(doc! { "patrimonio":{ "$regex": &query.unwrap() } }).run()?;
-                        
-                            /////
-                        for result in cursor {
-                            
-                            match result {
-                                Ok(value) => {
-                                    match value {
-                                        doc => {
+                            let query = Text::new("Patrimonio/modelo?").with_page_size(40).with_validator(validator)
+                                .prompt()
+                                .inspect_err(|_f| {
+                                    let _=main();
+                                });
+                            let cursor = coll.find(doc! { "patrimonio":&query.unwrap() }).run()?;
+
+                            for result in cursor {
+                                    match result {
+                                        Ok(doc) => {
+                                                
                                             std::process::Command::new("clear").status().unwrap();
                                             {
                                                 match doc.get("grupo").unwrap().as_null() {
-                                                    Some(_a) =>  println!(r"grupo:{color_green}...{color_reset}"),
-                                                    _ => println!(r"grupo:{color_green}{}{color_reset}",doc.get("grupo").unwrap().as_str().unwrap())
-                                                } 
+                                                        Some(_a) =>  println!(r"grupo:{color_green}...{color_reset}"),
+                                                        _ => println!(r"grupo:{color_green}{}{color_reset}",doc.get("grupo").unwrap().as_str().unwrap())
+                                                    } 
                                             }
                                             println!(r"modelo:{color_cyan}{}{color_reset}", doc.get("modelo").unwrap().as_str().unwrap());
                                             println!(r"patrimonio:{color_blue}{}{color_reset}",doc.get("patrimonio").unwrap().as_str().unwrap());
                                             println!(r"evento:{color_red}{}{color_reset} data:{color_red}{}{color_reset}user:{color_red}{}{color_reset}",doc.get("evento").unwrap().as_str().unwrap(),doc.get("data").unwrap().as_str().unwrap(),doc.get("user").unwrap().as_str().unwrap());
                                             {
                                                 if doc.get("ultimoevento").is_some(){
-
+                                        
                                                     println!(r"ultimo evento:{color_red}{}{color_reset}",doc.get("ultimoevento").unwrap().as_str().unwrap());
                                                 }else{
-                                                    println!(r"ultimo evento:{color_red}-----{color_reset}");
-
-                                                }
+                                                        println!(r"ultimo evento:{color_red}-----{color_reset}");
+                                                    }
                                             }
                                             println!(r"info:{color_red}{}{color_reset}",doc.get("info").unwrap().as_str().unwrap());
                                             println!(r"{color_red}-------------------------------------------------------------{color_reset}");
                                             let mut buffer = String::new();
                                             stdin().read_line(&mut buffer)?;
                                             std::process::Command::new("clear").status().unwrap();
-                                            continue 'begin;
-                                            
+                                            continue 'begin;     
+
+                                        },
+                                        Err(_err)=> {
                                         }
                                     }
-                                },
-                                _ => {
-                                    std::process::Command::new("clear").status().unwrap();
-                                    let _ =procurar(&coll);
-                                }
                             }
-                        }
-                        ]
+                            std::process::Command::new("clear").status().unwrap();
+                            continue 'begin;
 
+                        ]
                     },
                     _=>{
                         let cursor = coll.find(doc! { "modelo":{ "$regex": &choice.as_str() } }).run()?;
@@ -551,6 +624,8 @@ fn info(coll:Collection<Document> )-> Result<(), Box<dyn std::error::Error>>{
 // }
 
 fn imprimir(coll:Collection<Document> )-> Result<(), Box<dyn std::error::Error>>{
+    let skin = make_skin();
+    
     let font_family = genpdf::fonts::from_files("./fonts", "Sansation", None).expect("Failed to load font family");
     let mut doc = genpdf::Document::new(font_family);
     doc.set_title("Acap locacao");
@@ -622,20 +697,24 @@ fn imprimir(coll:Collection<Document> )-> Result<(), Box<dyn std::error::Error>>
                                 let mut gravar=String::new();
 
                                 for (k,v) in zip_document_array(result).iter(){
-                                    gravar.push_str(format!("{} | {} | {:?}\n", {
+                                    gravar.push_str(format!("{color_green}{}{color_reset} | {color_cyan}{}{color_reset} \n", {
                                         if v.len() < 10 {format!("0{}",v.len())}else{format!("{}",v.len())}
                                     },{
                                         format!("{}{}",k," ".repeat(60-k.len()))
-                                    },v).as_str());
+                                    }).as_str());
+                                    //v
                                     doc.push(genpdf::elements::Paragraph::new(format!("{} | {} | {:?}\n", {
                                         if v.len() < 10 {format!("0{}",v.len())}else{format!("{}",v.len())}
                                     },{
                                         format!("{}{}",k," ".repeat(60-k.len()))
                                     },v).as_str()));
                                 }
+                                
+                                let _=run_app(skin.to_owned() ,gravar.as_str());
+                                //
                                 let _=fs::write(path,gravar); 
                                 
-                                doc.render_to_file(format!("{evento}.pdf")).expect("Failed to write PDF file");
+                                // doc.render_to_file(format!("{evento}.pdf")).expect("Failed to write PDF file");
                               
                                 std::process::Command::new("clear").status().unwrap();
 
